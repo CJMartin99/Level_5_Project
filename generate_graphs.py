08eb13f0b9b4b9dd616332695e4751646e6e9141
CJMartin99 <2395521m@student.gla.ac.uk>
2023-03-27 19:10:27 +0100

    graph changes

--- generate_graphs.py
+++ generate_graphs.py
@@ -8,24 +8,62 @@ def text_strip(input_text):
     int_val = int(stripped_text)
     return int_val
 
+def calculate_avg_runtime(df,code_type):
+    df['avg_runtime'] = df[['runtime','runtime1','runtime2','runtime3','runtime4']].mean(axis=1)
+    df['code_type'] = code_type
+
+def create_comparison_graph(df,hardware):
+    cwd = os.getcwd()
+
+    sb.set_theme(style="whitegrid")
+    #ax = sb.boxplot(x="code_type",y="avg_runtime",data=df_laptop_original_newline)
+    ax = sb.swarmplot(x="code_type",y="runtimes",data=df,hue="code_type",size=2.5,legend=False)#,jitter=0.4)
+    ax.set_title("Test Instances Runtimes (" + hardware + ")")
+    ax.set_xlabel("Code Test Cases")
+    ax.set_ylabel("Runtime (milliseconds)")
+    plt.yscale("log")
+    #plt.ylim(ymin=0.1)
+    plt.xticks(rotation=0)
+
+    os.chdir(cwd+"/graphs")
+    plt.savefig(hardware + "_Runtimes_2.png")
+    plt.clf()
+    os.chdir(cwd)
+
+def create_speed_up_graph(df,hardware,instance):
+    cwd = os.getcwd()
+
+    sb.set_theme(style="whitegrid")
+    ax = sb.stripplot(x="file",y="speed_up",data=df,size=6,jitter=0.3)
+    ax.set_title(instance + " speed-up vs Original (" + hardware + ")")
+    ax.set_xlabel("Test instances")
+    ax.set_ylabel("Speed-Up")
+    plt.xticks(rotation=270)
+    plt.ylim(ymin=0)
+
+    os.chdir(cwd+"/graphs")
+    plt.savefig(instance + "_speedup_vs_original_" + hardware + ".png")
+    plt.clf()
+    os.chdir(cwd)
+
 def main():
     cwd = os.getcwd()
 
     os.chdir(cwd + "/results/results_0")
     df_laptop_original = pd.read_csv('Laptop_tests_Original.csv')
-    #df_cluster_original = pd.read_csv('Cluster_tests_Original.csv')
+    df_cluster_original = pd.read_csv('Cluster_tests_Original.csv')
     #df_epyc_original = pd.read_csv('Epyc_tests_Original.csv')
 
     df_laptop_newline = pd.read_csv('Laptop_tests_Newline.csv')
-    #df_cluster_newline = pd.read_csv('Cluster_tests_Newline.csv')
+    df_cluster_newline = pd.read_csv('Cluster_tests_Newline.csv')
     #df_epyc_newline = pd.read_csv('Epyc_tests_Newline.csv')
 
     df_laptop_fmt = pd.read_csv('Laptop_tests_FMT.csv')
-    #df_cluster_fmt = pd.read_csv('Cluster_tests_FMT.csv')
+    df_cluster_fmt = pd.read_csv('Cluster_tests_FMT.csv')
     #df_epyc_fmt = pd.read_csv('Epyc_tests_FMT.csv')
 
     df_laptop_cc = pd.read_csv('Laptop_tests_Colour_Class.csv')
-    #df_cluster_cc = pd.read_csv('Cluster_tests_Colour_Class.csv')
+    df_cluster_cc = pd.read_csv('Cluster_tests_Colour_Class.csv')
     #df_epyc_cc = pd.read_csv('Epyc_tests_Colour_Class.csv')
 
     # final version will be set to 10
@@ -36,9 +74,9 @@ def main():
         laptop_original = laptop_original['runtime']
         df_laptop_original['runtime'+str(i)] = laptop_original
 
-        #cluster_original = pd.read_csv('Cluster_tests_Original.csv')
-        #cluster_original = cluster_original['runtime']
-        #df_cluster_original['runtime'+str(i)] = cluster_original
+        cluster_original = pd.read_csv('Cluster_tests_Original.csv')
+        cluster_original = cluster_original['runtime']
+        df_cluster_original['runtime'+str(i)] = cluster_original
 
         #epyc_original = pd.read_csv('Epyc_tests_Original.csv')
         #epyc_original = epyc_original['runtime']
@@ -48,9 +86,9 @@ def main():
         laptop_newline = laptop_newline['runtime']
         df_laptop_newline['runtime'+str(i)] = laptop_newline
         
-        #cluster_newline = pd.read_csv('Cluster_tests_Newline.csv')
-        #cluster_newline = cluster_newline['runtime']
-        #df_cluster_newline['runtime'+str(i)] = cluster_newline
+        cluster_newline = pd.read_csv('Cluster_tests_Newline.csv')
+        cluster_newline = cluster_newline['runtime']
+        df_cluster_newline['runtime'+str(i)] = cluster_newline
         
         #epyc_newline = pd.read_csv('Epyc_tests_Newline.csv')
         #epyc_newline = epyc_newline['runtime']
@@ -60,9 +98,9 @@ def main():
         laptop_fmt = laptop_fmt['runtime']
         df_laptop_fmt['runtime'+str(i)] = laptop_fmt
         
-        #cluster_fmt = pd.read_csv('Cluster_tests_FMT.csv')
-        #cluster_fmt = cluster_fmt['runtime']
-        #df_cluster_fmt['runtime'+str(i)] = cluster_fmt
+        cluster_fmt = pd.read_csv('Cluster_tests_FMT.csv')
+        cluster_fmt = cluster_fmt['runtime']
+        df_cluster_fmt['runtime'+str(i)] = cluster_fmt
         
         #epyc_fmt = pd.read_csv('Epyc_tests_FMT.csv')
         #epyc_fmt = epyc_fmt['runtime']
@@ -72,9 +110,9 @@ def main():
         laptop_cc = laptop_cc['runtime']
         df_laptop_cc['runtime'+str(i)] = laptop_cc
         
-        #cluster_cc = pd.read_csv('Cluster_tests_Colour_Class.csv')
-        #cluster_cc = cluster_cc['runtime']
-        #df_cluster_cc['runtime'+str(i)] = cluster_cc
+        cluster_cc = pd.read_csv('Cluster_tests_Colour_Class.csv')
+        cluster_cc = cluster_cc['runtime']
+        df_cluster_cc['runtime'+str(i)] = cluster_cc
         
         #epyc_cc = pd.read_csv('Epyc_tests_Colour_Class.csv')
         #epyc_cc = epyc_cc['runtime']
@@ -82,76 +120,56 @@ def main():
 
     os.chdir(cwd)
 
-    print(df_laptop_original.head())
+    print(df_cluster_original.head())
 
     # Average the runtimes for each test case on each hardware
     # code for expanding back to 10 runs
     # ,'runtime5','runtime6','runtime7','runtime8','runtime9'
+    for df in [df_laptop_original,df_cluster_original]:
+        calculate_avg_runtime(df,"original")
 
-    df_laptop_original['avg_runtime'] = df_laptop_original[['runtime','runtime1','runtime2','runtime3','runtime4']].mean(axis=1)
-    df_laptop_original['code_type'] = 'original'
-    #df_cluster_original_mean = df_cluster_original[['runtime','runtime1','runtime2','runtime3','runtime4','runtime5','runtime6','runtime7','runtime8','runtime9']].mean(axis=1)
-    #df_epyc_original_mean = df_epyc_original[['runtime','runtime1','runtime2','runtime3','runtime4','runtime5','runtime6','runtime7','runtime8','runtime9']].mean(axis=1)
-     
-    df_laptop_newline['avg_runtime'] = df_laptop_newline[['runtime','runtime1','runtime2','runtime3','runtime4']].mean(axis=1)
-    df_laptop_newline['code_type'] = 'newline'
-    #df_cluster_newline_mean = df_cluster_newline[['runtime','runtime1','runtime2','runtime3','runtime4','runtime5','runtime6','runtime7','runtime8','runtime9']].mean(axis=1)
-    #df_epyc_newline_mean = df_epyc_newline[['runtime','runtime1','runtime2','runtime3','runtime4','runtime5','runtime6','runtime7','runtime8','runtime9']].mean(axis=1)
-    
-    df_laptop_fmt['avg_runtime'] = df_laptop_fmt[['runtime','runtime1','runtime2','runtime3','runtime4']].mean(axis=1)
-    df_laptop_fmt['code_type'] = 'fmt'
-    #df_cluster_fmt_mean = df_cluster_fmt[['runtime','runtime1','runtime2','runtime3','runtime4','runtime5','runtime6','runtime7','runtime8','runtime9']].mean(axis=1)
-    #df_epyc_fmt_mean = df_epyc_fmt[['runtime','runtime1','runtime2','runtime3','runtime4','runtime5','runtime6','runtime7','runtime8','runtime9']].mean(axis=1)
-    
-    df_laptop_cc['avg_runtime'] = df_laptop_cc[['runtime','runtime1','runtime2','runtime3','runtime4']].mean(axis=1)
-    df_laptop_cc['code_type'] = 'colour_class'
-    #df_cluster_cc_mean = df_cluster_cc[['runtime','runtime1','runtime2','runtime3','runtime4','runtime5','runtime6','runtime7','runtime8','runtime9']].mean(axis=1)
-    #df_epyc_cc_mean = df_epyc_cc[['runtime','runtime1','runtime2','runtime3','runtime4','runtime5','runtime6','runtime7','runtime8','runtime9']].mean(axis=1)
+    for df in [df_laptop_newline,df_cluster_newline]:
+        calculate_avg_runtime(df,"newline")
+
+    for df in [df_laptop_fmt,df_cluster_fmt]:
+        calculate_avg_runtime(df,"fmt")
+
+    for df in [df_laptop_cc,df_cluster_cc]:
+        calculate_avg_runtime(df,"colour_class")
 
     # create merged dfs for each graph
-    df_laptop_original_newline = pd.concat([df_laptop_original, df_laptop_newline])
-    df_laptop_original_newline = pd.melt(df_laptop_original_newline,
-                                         id_vars=["file","code_type"],
-                                         value_vars=["runtime","runtime1","runtime2","runtime3","runtime4"],
-                                         var_name = "test_run",
-                                         value_name="runtimes")
+    df_laptop = pd.concat([df_laptop_original,df_laptop_newline,df_laptop_fmt,df_laptop_cc])
+    df_laptop = pd.melt(df_laptop,
+                            id_vars=["file","code_type"],
+                            value_vars=["runtime","runtime1","runtime2","runtime3","runtime4"],
+                            var_name = "test_run",
+                            value_name="runtimes")
+    
+    df_cluster = pd.concat([df_cluster_original,df_cluster_newline,df_cluster_fmt,df_cluster_cc])
+    df_cluster = pd.melt(df_cluster,
+                            id_vars=["file","code_type"],
+                            value_vars=["runtime","runtime1","runtime2","runtime3","runtime4"],
+                            var_name = "test_run",
+                            value_name="runtimes")
 
     df_laptop_newline["speed_up"] = df_laptop_original['avg_runtime'] / df_laptop_newline['avg_runtime']
-
-    df_laptop_original_fmt = pd.concat([df_laptop_original, df_laptop_fmt])
     df_laptop_fmt["speed_up"] = df_laptop_original['avg_runtime'] / df_laptop_fmt['avg_runtime']
-
-    df_laptop_newline_fmt = pd.concat([df_laptop_newline, df_laptop_fmt])
-    df_laptop_newline["speed_up_fmt"] = df_laptop_fmt['avg_runtime'] / df_laptop_newline['avg_runtime']
-
-    df_laptop_original_cc = pd.concat([df_laptop_original, df_laptop_cc])
     df_laptop_cc["speed_up"] = df_laptop_original['avg_runtime'] / df_laptop_cc['avg_runtime']
+
+    df_cluster_newline["speed_up"] = df_cluster_original['avg_runtime'] / df_cluster_newline['avg_runtime']
+    df_cluster_fmt["speed_up"] = df_cluster_original['avg_runtime'] / df_cluster_fmt['avg_runtime']
+    df_cluster_cc["speed_up"] = df_cluster_original['avg_runtime'] / df_cluster_cc['avg_runtime']
     
     # generate graphs for each hardware comparing the different test cases - CHANGE to all runtimes rather than average
-    sb.set_theme(style="whitegrid")
-    #ax = sb.boxplot(x="code_type",y="avg_runtime",data=df_laptop_original_newline)
-    ax = sb.stripplot(x="code_type",y="runtimes",data=df_laptop_original_newline,hue="code_type",size=6,jitter=0.3)
-    ax.set_title("Test Instances Average Runtimes")
-    ax.set_xlabel("Test instances")
-    ax.set_ylabel("Runtime (milliseconds)")
-    plt.yscale("log")
-    plt.ylim(ymin=0.0001)
-    plt.xticks(rotation=90)
-    plt.savefig("original_vs_newline_2.png")
-    #plt.show()
-    plt.clf()
-    # generate speed up graphs
-    sb.set_theme(style="whitegrid")
-    ax = sb.stripplot(x="file",y="speed_up",data=df_laptop_cc,size=6,jitter=0.3)
-    ax.set_title("Speed-up per Instance on avg runtimes")
-    ax.set_xlabel("Test instances")
-    ax.set_ylabel("Speed-Up")
-    plt.xticks(rotation=90)
-    plt.savefig("newline_speedup_vs_original.png")
-    #plt.show()
-
-    # generate graphs for special cases to examine
-
+    create_comparison_graph(df_laptop,"Laptop")
+    create_comparison_graph(df_cluster,"Cluster")
+    #plt.clf()
+    create_speed_up_graph(df_laptop_newline,"Laptop","Newline")
+    create_speed_up_graph(df_laptop_fmt,"Laptop","FMT")
+    create_speed_up_graph(df_laptop_cc,"Laptop","Colour_Class")
+    create_speed_up_graph(df_cluster_newline,"Cluster","Newline")
+    create_speed_up_graph(df_cluster_fmt,"Cluster","FMT")
+    create_speed_up_graph(df_cluster_cc,"Cluster","Colour_Class")
     
 if __name__ == "__main__":
     main()
